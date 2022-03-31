7beb55f1ed8ce02d7c45f57b43ae41c208cff124
Tim Rieber <tim.rieber@t-online.de>
2022-03-31 22:00:12 +0200

    implementation of synthetic identities

--- training/dataset.py
+++ training/dataset.py
@@ -137,13 +137,14 @@ class Dataset(torch.utils.data.Dataset):
 
     @property
     def label_shape(self):
-        if self._label_shape is None:
-            raw_labels = self._get_raw_labels()
-            if raw_labels.dtype == np.int64:
-                self._label_shape = [int(np.max(raw_labels)) + 1]
-            else:
-                self._label_shape = raw_labels.shape[1:]
-        return list(self._label_shape)
+        # if self._label_shape is None:
+        #     raw_labels = self._get_raw_labels()
+        #     if raw_labels.dtype == np.int64:
+        #         self._label_shape = [int(np.max(raw_labels)) + 1]
+        #     else:
+        #         self._label_shape = raw_labels.shape[1:]
+        # return list(self._label_shape)
+        return list([10672])
 
     @property
     def label_dim(self):
