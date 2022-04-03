45646911ad139b94dea2c6db0289dff6c0ada94f
Tim Rieber <tim.rieber@t-online.de>
2022-04-03 22:06:44 +0200

    added casia image info txt

--- tim_tools/casia_path_list.py
+++ tim_tools/casia_path_list.py
@@ -1,5 +1,5 @@
 from os import listdir
-path = "/data/fboutros/faces_webface_112x112"
+path = "/data/fboutros/faces_webface_112x112/casia_training"
 with open("image_path_list.txt", "w") as file:
     for i in range(0, 10572):
         images = [f for f in listdir(path + "/" + "{:0>6d}".format(i))]
