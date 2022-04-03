9b4561ddc5d46753f60a3c42c0a22ea1c3f7d29c
Tim Rieber <tim.rieber@t-online.de>
2022-04-03 22:04:34 +0200

    added casia image info txt

--- tim_tools/casia_path_list.py
+++ tim_tools/casia_path_list.py
@@ -1,7 +1,9 @@
-path = "" # "~/Synthetic-SG2/generated/sg2-casia-cond/"
+from os import listdir
+path = "/data/fboutros/faces_webface_112x112"
 with open("image_path_list.txt", "w") as file:
     for i in range(0, 10572):
-        for j in range (1, 16):
-            file.write("{:0>6d}".format(i) + "/" + "{:0>8d}".format(j) + ".jpg")
-            file.write("\n")
+        images = [f for f in listdir(path + "/" + "{:0>6d}".format(i))]
+        for image in images:
+            file.write("{:0>6d}".format(i) + "/" + image.split(".")[0])
+
 file.close()
