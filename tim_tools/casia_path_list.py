6042be31d69641b96cc74cda4cedb09c1b4db92c
Tim Rieber <tim.rieber@t-online.de>
2022-04-03 22:08:45 +0200

    added casia image info txt

--- tim_tools/casia_path_list.py
+++ tim_tools/casia_path_list.py
@@ -4,6 +4,6 @@ with open("image_path_list.txt", "w") as file:
     for i in range(0, 10572):
         images = [f for f in listdir(path + "/" + "{:0>6d}".format(i))]
         for image in images:
-            file.write("{:0>6d}".format(i) + "/" + image.split(".")[0])
-
+            file.write("{:0>6d}".format(i) + "/" + image)
+            file.write("\n")
 file.close()
