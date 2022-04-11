745e6580d4176b3fa21a05cbdda2a2353db9a49d
Tim Rieber <tim.rieber@t-online.de>
2022-04-11 11:45:54 +0200

    fixes to IDnet addition

--- training/loss.py
+++ training/loss.py
@@ -81,7 +81,7 @@ class StyleGAN2Loss(Loss):
                 training_stats.report('Loss/signs/fake', gen_logits.sign())
                 pred_classes = self.run_IDNet(gen_img, gen_c, sync=False)
                 loss_disc = torch.nn.functional.softplus(-gen_logits)
-                loss_id = 0.1 * self.idnet_loss(pred_classes, torch.argmax(gen_c, dim=1))
+                loss_id = 0.05 * self.idnet_loss(pred_classes, torch.argmax(gen_c, dim=1))
                 loss_Gmain = loss_disc + loss_id
                 training_stats.report('Loss/G/disc', loss_disc)
                 training_stats.report('Loss/G/id', loss_id)
