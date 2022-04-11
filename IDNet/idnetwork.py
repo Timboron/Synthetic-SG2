defa370009ea3c58b4f31c4151066488b75d9028
Tim Rieber <tim.rieber@t-online.de>
2022-04-11 10:09:47 +0200

    fixes to IDnet addition

--- IDNet/idnetwork.py
+++ IDNet/idnetwork.py
@@ -1,4 +1,5 @@
 from torch import nn
+from torchvision import transforms
 
 from IDNet.backbones.iresnet import iresnet50
 from IDNet.loss import CosFace
@@ -9,8 +10,10 @@ class IDNet(nn.Module):
         super(IDNet, self).__init__()
         self.backbone = iresnet50(dropout=0.4, num_features=embedding_size)
         self.cosface = CosFace(embedding_size, c_dim)
+        self.resize = nn.Sequential(transforms.Resize(112))
 
     def forward(self, images, labels):
-        embeddings = self.backbone(images)
+        resized_images = self.resize(images)
+        embeddings = self.backbone(resized_images)
         ret = self.cosface(embeddings, labels)
         return ret
