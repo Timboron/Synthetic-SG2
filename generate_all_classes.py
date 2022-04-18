5f39403d01c9cbfcbc7a9c026279099dc188bfe4
Tim Rieber <tim.rieber@t-online.de>
2022-04-18 17:55:05 +0200

    fixes

--- generate_all_classes.py
+++ generate_all_classes.py
@@ -73,7 +73,7 @@ def generate_images(
             z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
             img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
             img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
-            PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/{class_idx}/{seed_idx:06d}.png')
+            PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/{class_idx:06d}/{seed_idx:06d}.png')
 
 
 #----------------------------------------------------------------------------
--- tim_tools/casia_path_list.py
+++ tim_tools/casia_path_list.py
@@ -4,6 +4,7 @@ with open("../image_path_list.txt", "w") as file:
     for i in range(0, 10572):
         images = [f for f in listdir(path + "/" + str(i))]  # "{:0>6d}".format(i))]
         for image in images:
-            file.write("{:0>6d}".format(i) + "/" + image)
+            #file.write("{:0>6d}".format(i) + "/" + image)
+            file.write(str(i) + "/" + image)
             file.write("\n")
 file.close()
