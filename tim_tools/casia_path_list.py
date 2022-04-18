034eb0348ad4ae4d63190f583f30e5f6c61a2b7d
Tim Rieber <tim.rieber@t-online.de>
2022-04-18 17:51:31 +0200

    fixes

--- tim_tools/casia_path_list.py
+++ tim_tools/casia_path_list.py
@@ -1,6 +1,6 @@
 from os import listdir
-path = "/data/fboutros/faces_webface_112x112/casia_training"
-with open("image_path_list.txt", "w") as file:
+path = "/home/trieber/Synthetic-SG2/generated/sg2_cond_idnet_resume"
+with open("../image_path_list.txt", "w") as file:
     for i in range(0, 10572):
         images = [f for f in listdir(path + "/" + str(i))]  # "{:0>6d}".format(i))]
         for image in images:
