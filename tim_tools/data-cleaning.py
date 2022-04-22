be2d420c87d78ba4101fdab46f7fa9c3125fd154
Tim Rieber <tim.rieber@t-online.de>
2022-04-22 11:13:59 +0200

    data cleaning

---
+++ tim_tools/data-cleaning.py
@@ -0,0 +1,48 @@
+import numpy as np
+import json
+from tqdm import tqdm
+import os
+
+import click
+
+def similarity(emb0, emb1):
+    if emb0.ndim < 2 or emb1.ndim < 2:
+        print("ERROR")
+        print(emb0, emb1)
+        return []
+    assert (emb0.shape[0] == emb1.shape[0])
+    assert (emb0.shape[1] == emb1.shape[1])
+    dot = np.sum(np.multiply(emb0, emb1), axis=1)
+    norm = np.linalg.norm(emb0, axis=1) * np.linalg.norm(emb1, axis=1)
+    sim = np.clip(dot / norm, -1., 1.)
+    return sim
+
+
+@click.command()
+@click.pass_context
+@click.option('--path', 'pathname', required=True)
+def generate_match(pathname: str):
+    with open('/home/trieber/data_cleaning/test.json', 'w') as outfile:
+        json.dump({"test": 0}, outfile)
+
+    identities = os.listdir(pathname)
+    image_genuine_scores = {}
+
+    for identity in tqdm(identities):
+        id_path = pathname + "/" + identity
+        if not os.path.exists(id_path):
+            continue
+        features = [np.load(x) for x in [id_path + "/" + y for y in os.listdir(id_path) if ".npy" in y]]
+
+        for feature_idx, feature in enumerate(features):
+            references = np.vstack(([feature] * len(features)))
+            probes = np.vstack(features)
+            gen_score = similarity(references, probes)
+            image_genuine_scores[str(identity) + "/" + str(feature_idx)] = sum(gen_score) / len(gen_score)
+
+    score_sorted = {k: v for k, v in sorted(image_genuine_scores.items(), key=lambda item: item[1])}
+    with open('/home/trieber/data_cleaning/genuine_image_scores.json', 'w') as outfile:
+        json.dump(score_sorted, outfile)
+
+if __name__ == "__main__":
+    generate_match()
