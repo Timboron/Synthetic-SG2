2f888def4e0a0cbe58dbb744de695a08304c30e0
Tim Rieber <tim.rieber@t-online.de>
2022-04-22 11:39:36 +0200

    path list

--- tim_tools/image_path_list.py
+++ tim_tools/image_path_list.py
@@ -1,5 +1,5 @@
 from os import listdir
-path = "/data/trieber/syn_datasets/sg2_cond"
+path = "/data/trieber/syn_datasets/sg2cond"
 with open("/data/trieber/syn_datasets/image_path_list.txt", "w") as file:
     for i in range(0, 10572):
         images = [f for f in listdir(path + "/" + "{:0>6d}".format(i))]
