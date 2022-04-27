ea716593a90c514b0859b56621ed83e7126a61e5
Tim Rieber <tim.rieber@t-online.de>
2022-04-27 19:16:23 +0200

    path list

---
+++ tim_tools/empty_labels.py
@@ -0,0 +1,22 @@
+import os
+import click
+
+
+@click.command()
+@click.pass_context
+@click.option('--path', 'pathname', required=True)
+def generate_match(ctx: click.Context, pathname: str):
+
+    identities = os.listdir(pathname)
+    empty_labels = []
+
+    for identity in identities:
+        images = os.listdir(os.path.join(pathname, identity))
+        if len(images) == 0:
+            empty_labels.append(identity)
+
+    print(empty_labels)
+
+
+if __name__ == "__main__":
+    generate_match()
