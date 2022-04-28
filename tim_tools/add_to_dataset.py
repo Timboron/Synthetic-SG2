82bd7dbc313a2c07a4e492769320136e16140294
Tim Rieber <tim.rieber@t-online.de>
2022-04-28 23:48:32 +0200

    dataclean

--- tim_tools/add_to_dataset.py
+++ tim_tools/add_to_dataset.py
@@ -15,7 +15,7 @@ def generate_match(ctx: click.Context, inpath: str, outpath: str, before: int, a
 
     for identity in tqdm(identities):
         id_path = os.path.join(inpath, identity)
-        if not os.path.exists(outpath, identity):
+        if not os.path.exists(os.path.join(outpath, identity)):
             os.makedirs(id_path)
         images = os.listdir(id_path)
         for idx, image in enumerate(images):
