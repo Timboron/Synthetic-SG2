63a879997b4215cf068741316d99a3c508907a11
Tim Rieber <tim.rieber@t-online.de>
2022-04-28 23:43:23 +0200

    dataclean

---
+++ tim_tools/add_to_dataset.py
@@ -0,0 +1,27 @@
+import shutil
+from tqdm import tqdm
+import os
+
+import click
+
+@click.command()
+@click.pass_context
+@click.option('--inpath', 'inpath', required=True)
+@click.option('--outpath', 'outpath', required=True)
+@click.option('--before', 'before', default=-1)
+@click.option('--after', 'after', required=True)
+def generate_match(ctx: click.Context, inpath: str, outpath: str, before: int, after: int):
+    identities = os.listdir(inpath)
+
+    for identity in tqdm(identities):
+        id_path = os.path.join(inpath, identity)
+        if not os.path.exists(id_path):
+            os.makedirs(id_path)
+        images = os.listdir(id_path)
+        for idx, image in enumerate(images):
+            if before < idx <= after:
+                shutil.copyfile(os.path.join(id_path, image), os.path.join(outpath, identity, image))
+
+
+if __name__ == "__main__":
+    generate_match()
