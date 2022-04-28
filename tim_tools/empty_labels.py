40545b82d27e10736b95c0978003d5df0b067ffe
Tim Rieber <tim.rieber@t-online.de>
2022-04-28 14:13:19 +0200

    dataclean

--- tim_tools/empty_labels.py
+++ tim_tools/empty_labels.py
@@ -9,16 +9,19 @@ def generate_match(ctx: click.Context, pathname: str):
 
     identities = os.listdir(pathname)
     empty_labels = []
+    one_labels = []
 
     for identity in identities:
         images = os.listdir(os.path.join(pathname, identity))
         if len(images) == 0:
             empty_labels.append(identity)
+        if len(images) == 1:
+            one_labels.append(identity)
         if len(images) < 200:
             print(len(images))
 
-    print(len(empty_labels))
-
+    print("empty:",len(empty_labels))
+    print("one:", len(one_labels))
 
 if __name__ == "__main__":
     generate_match()
