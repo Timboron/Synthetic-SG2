7505e0be942fefa6dce2e5896f6433a496e38e4a
Tim Rieber <tim.rieber@t-online.de>
2022-05-03 07:17:13 +0200

    add intraclass variance check

--- tim_tools/image_path_list.py
+++ tim_tools/image_path_list.py
@@ -1,6 +1,6 @@
 from os import listdir
-path = "/data/trieber/syn_datasets/sg2idbn_aligned"
-with open("path_list_sg2idbn_align.txt", "w") as file:
+path = "/data/trieber/syn_datasets/sg2idbnee_500K_aligned"
+with open("path_list_sg2idbnee_500K_aligned.txt", "w") as file:
     for i in range(0, 10572):
         images = [f for f in listdir(path + "/" + "{:0>6d}".format(i))]
         for image in images:
